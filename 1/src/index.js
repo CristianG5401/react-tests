c362dde7399c140a19cee7fe8a86dd68f75e259e
unknown <cristianneatproy@gmail.com>
2018-10-10 00:16:33 -0500

    added component with jsx

--- 1/src/index.js
+++ 1/src/index.js
@@ -4,8 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import _ from 'lodash'
 
-const e = React.createElement
-
+/* ************************************* Component using JSX ************************************** */
 class LikeButton extends React.Component {
   constructor (props) {
     super(props)
@@ -25,5 +24,32 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container')
+const domContainer = document.querySelector('#like_button_container_jsx')
 ReactDOM.render(<LikeButton />, domContainer)
+/* *************************************************************************** */
+
+/* ************************************* Component without JSX ************************************** */
+
+const e = React.createElement
+
+class LikeButtonNoJsx extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { liked: false }
+  }
+
+  render () {
+    if (this.state.liked) {
+      return 'You liked this 2.'
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like 2'
+    )
+  }
+}
+
+const domContainer2 = document.querySelector('#like_button_container')
+ReactDOM.render(e(LikeButtonNoJsx), domContainer2)
